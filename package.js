1b82e3e872cbad3e4fb35f7bc17337eaa87e5d83
root <mcbrumagin@gmail.com>
2015-07-17 01:05:58 -0400

    Added jquery version, package added to atmosphere.

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 
 Package.describe({
+  name: 'mcbrumagin:jquery-requestanimationframe',
   summary: 'jquery-requestAnimationFrame gnarf37 Github package',
   version: '0.1.2',
-  name: 'jquery-requestanimationframe',
   documentation: 'README.md',
   git: 'https://github.com/mcbrumagin/Meteor-jquery-requestAnimationFrame.git'
 })
 
 Package.onUse(function(api) {
-  api.imply('jquery', 'client')
+  api.imply('jquery@1.11.3_2', 'client')
   api.addFiles(['jquery.requestAnimationFrame.js'], 'client')
 })
+
